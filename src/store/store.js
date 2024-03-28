fc57b8d2c56ff687a23e59af900d9e596bb18cae
Dhanajava <dhanasneka010@gmail.com>
2024-03-28 17:40:24 +0530

    toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,45 +1,47 @@
-import { compose, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { compose, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
-import { legacy_createStore as createStore } from 'redux'
+// import { legacy_createStore as createStore } from 'redux'
 import { rootReducer } from './root-reducer';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import { rootSaga } from './root-saga';
-// import {thunk} from 'redux-thunk'
+// import createSagaMiddleware from 'redux-saga';
+// import { rootSaga } from './root-saga';
+// // import {thunk} from 'redux-thunk'
 
 
 
-const persistConfig = {
-  key:'root',
-  storage,
-  whitelist:['cart']
-};
+// const persistConfig = {
+//   key:'root',
+//   storage,
+//   whitelist:['cart']
+// };
 
-const sagaMiddleware = createSagaMiddleware();
+// const sagaMiddleware = createSagaMiddleware();
 
-const persistedReducer = persistReducer(persistConfig,rootReducer)
+// const persistedReducer = persistReducer(persistConfig,rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware ].filter(
+const middleWares = [process.env.NODE_ENV !== 'development' && logger].filter(
     Boolean
  );
 
-const composeEnhancer = 
- ( process.env.NODE_ENV !== 'production' &&
-   window &&
-   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || 
-  compose;
+// const composeEnhancer = 
+//  ( process.env.NODE_ENV !== 'production' &&
+//    window &&
+//    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || 
+//   compose;
 
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
-export const store =  createStore(
-    persistedReducer,
-    undefined, 
-    composedEnhancers 
-    );
+ export const store = configureStore({
+   reducer:rootReducer,
+   middleware:(getDefaultMiddleware) =>
+   getDefaultMiddleware({
+      serializableCheck:false,
+   }).concat(middleWares)
+})
 
-    sagaMiddleware.run(rootSaga);
 
-export const persistor = persistStore(store)    
+// export const persistor = persistStore(store)    
